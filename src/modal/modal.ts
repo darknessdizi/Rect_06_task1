aadb8b9436ab6ef220f3e07f56b328e8051338ff
Remezov Dmitriy <darknessdizi@gmail.com>
2024-08-15 23:28:08 +0700

    callback тип any

--- src/modal/modal.ts
+++ src/modal/modal.ts
@@ -12,13 +12,15 @@ export interface IClockProps {
     title: string,
     zone: string,
   })[],
-  callback: (event: React.ChangeEvent<HTMLElement>) => void,
+  // callback: (event: React.ChangeEvent<HTMLElement>) => void,
+  callback: any,
 }
 
 export interface IItemClockProps {
   title: string,
   zone: string,
-  callback: (event: React.ChangeEvent<HTMLElement>) => void,
+  // callback: (event: React.ChangeEvent<HTMLElement>) => void,
+  callback: any,
 }
 
 export interface IFormProps {
